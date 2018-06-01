5ca66132c231df634d061c891a583b0d49140a34
Van Pho Nguyen <vanpho02@gmail.com>
2018-06-01 20:07:10 +0700

    getDbUri

--- src/helpers/connectDatabase.js
+++ src/helpers/connectDatabase.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/mean0705x', { useMongoClient: true })
+function getDatabaseUri() {
+    if (process.env.NODE_ENV === 'production') return '';
+    if (process.env.NODE_ENV === 'test') return 'mongodb://localhost/mean0705x-test';
+    return 'mongodb://localhost/mean0705x';
+}
+
+mongoose.connect(getDatabaseUri(), { useMongoClient: true })
 .then(() => console.log('Database connect'))
 .catch(error => {
     console.log('Cannot connect database', error);
--- test/test-helper.js
+++ test/test-helper.js
@@ -1,1 +1,2 @@
+process.env.NODE_ENV = 'test';
 require('../src/helpers/connectDatabase');
