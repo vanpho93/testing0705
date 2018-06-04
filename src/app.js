4e4aff36d7c96508809f26a69b731ed6a49293e0
Van Pho Nguyen <vanpho02@gmail.com>
2018-06-04 21:20:09 +0700

    res onerror

--- src/app.js
+++ src/app.js
@@ -6,6 +6,14 @@ const { User } = require('./models/user.model');
 const app = express();
 app.use(json());
 
+app.use((req, res, next) => {
+    res.onError = error => {
+        if (!error.status) console.log(error);
+        res.status(error.status || 500).send({ success: false, message: error.message });
+    }
+    next();
+});
+
 app.get('/user', (req, res) => {
     User.find({})
     .then(users => res.send({ success: true, users }));
@@ -15,21 +23,21 @@ app.post('/user/signup', (req, res) => {
     const { name, email, password } = req.body;
     UserService.signUp(name, email, password)
     .then(user => res.send({ success: true, user }))
-    .catch(error => res.status(error.status).send({ success: false, message: error.message }));
+    .catch(res.onError);
 });
 
 app.post('/user/signin', (req, res) => {
     const { email, password } = req.body;
     UserService.signIn(email, password)
     .then(user => res.send({ success: true, user }))
-    .catch(error => res.status(error.status).send({ success: false, message: error.message }));
+    .catch(res.onError);
 });
 
 app.post('/user/check', (req, res) => {
     const { token } = req.body;
     UserService.checkToken(token)
     .then(user => res.send({ success: true, user }))
-    .catch(error => res.status(error.status).send({ success: false, message: error.message }));
+    .catch(res.onError);
 });
 
 module.exports = { app };
