cf099abb878e5f4ecfcfcdd066207d281eeac062
Van Pho Nguyen <vanpho02@gmail.com>
2018-06-06 20:00:21 +0700

    id user

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const { json } = require('body-parser');
 const { UserService } = require('./services/user.service');
+const { StoryService } = require('./services/story.service');
 const { User } = require('./models/user.model');
+const { Story } = require('./models/story.model');
+const { verify } = require('./helpers/jwt');
 
 const app = express();
 app.use(json());
@@ -39,6 +42,22 @@ app.post('/user/check', (req, res) => {
     UserService.checkToken(token)
     .then(user => res.send({ success: true, user }))
     .catch(res.onError);
+});
+
+app.get('/story', (req, res) => {
+    Story.find({})
+    .then(stories => res.send({ success: true, stories }));
+});
+
+app.post('/story', async (req, res) => {
+    try {
+        const { content, token } = req.body;
+        const { _id } = await verify(token);
+        const story = await StoryService.createStory(_id, content);
+        res.send({ success: true, story });
+    } catch (error) {
+        res.onError(error);
+    }
 });
 
 module.exports = { app };
