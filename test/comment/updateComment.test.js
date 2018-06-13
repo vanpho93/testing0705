66b4b953309fa847f8e797be1fb48b1f19fa0812
Van Pho Nguyen <vanpho02@gmail.com>
2018-06-13 21:14:10 +0700

    x

--- test/comment/updateComment.test.js
+++ test/comment/updateComment.test.js
@@ -1,7 +1,7 @@
 const { equal } = require('assert');
 const request = require('supertest');
 const { app } = require('../../src/app');
-const { User } = require('../../src/models/user.model');
+const { Comment } = require('../../src/models/comment.model');
 const { Story } = require('../../src/models/story.model');
 const { UserService } = require('../../src/services/user.service');
 const { StoryService } = require('../../src/services/story.service');
@@ -21,7 +21,7 @@ describe('PUT /comment/:_id', () => {
         idUser2 = user2._id;
         const story = await StoryService.createStory(idUser1, 'abcd');
         idStory = story._id;
-        const comment = await CommentService.createComment(idUser2, idStory, 'abcd');
+        const comment = await CommentService.createComment(idUser2, idStory, 'ghi');
         idComment = comment._id;
     });
 
@@ -30,6 +30,69 @@ describe('PUT /comment/:_id', () => {
             .put('/comment/' + idComment)
             .send({ content: 'xyz' })
             .set({ token: token2 });
-        console.log(response.body);
+            equal(response.status, 200);
+        const { success, comment, message } = response.body;
+        equal(success, true);
+        equal(message, undefined);
+        equal(comment.content, 'xyz');
+        const commentDb = await Comment.findOne({}).populate('author');
+        equal(commentDb.content, 'xyz');
+    });
+
+    it('Cannot update comment with invalid id', async () => {
+        const response = await request(app)
+            .put('/comment/123')
+            .send({ content: 'xyz' })
+            .set({ token: token2 });
+        equal(response.status, 400);
+        const { success, comment, message } = response.body;
+        equal(success, false);
+        equal(message, 'INVALID_ID');
+        equal(comment, undefined);
+        const commentDb = await Comment.findOne({}).populate('author');
+        equal(commentDb.content, 'ghi');
+    });
+
+    it('Cannot update comment with invalid token', async () => {
+        const response = await request(app)
+            .put('/comment/' + idComment)
+            .send({ content: 'xyz' })
+            .set({ token: 'a.b.c' });
+        equal(response.status, 400);
+        const { success, comment, message } = response.body;
+        equal(success, false);
+        equal(message, 'INVALID_TOKEN');
+        equal(comment, undefined);
+        const commentDb = await Comment.findOne({}).populate('author');
+        equal(commentDb.content, 'ghi');
+    });
+
+    it('Cannot update comment with token-1', async () => {
+        const response = await request(app)
+            .put('/comment/' + idComment)
+            .send({ content: 'xyz' })
+            .set({ token: token1 });
+        equal(response.status, 404);
+        const { success, comment, message } = response.body;
+        equal(success, false);
+        equal(message, 'CANNOT_FIND_COMMENT');
+        equal(comment, undefined);
+        const commentDb = await Comment.findOne({}).populate('author');
+        equal(commentDb.content, 'ghi');
+    });
+
+    it('Cannot update removed comment', async () => {
+        await Comment.findByIdAndRemove(idComment);
+        const response = await request(app)
+            .put('/comment/' + idComment)
+            .send({ content: 'xyz' })
+            .set({ token: token2 });
+        equal(response.status, 404);
+        const { success, comment, message } = response.body;
+        equal(success, false);
+        equal(message, 'CANNOT_FIND_COMMENT');
+        equal(comment, undefined);
+        const commentDb = await Comment.findOne({}).populate('author');
+        equal(commentDb, null);
     });
 });
