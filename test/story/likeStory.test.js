f255f5f122db6816df13db4825f8fb046a0b4a0e
Van Pho Nguyen <vanpho02@gmail.com>
2018-06-15 20:06:00 +0700

    like

--- test/story/likeStory.test.js
+++ test/story/likeStory.test.js
@@ -6,7 +6,7 @@ const { Story } = require('../../src/models/story.model');
 const { UserService } = require('../../src/services/user.service');
 const { StoryService } = require('../../src/services/story.service');
 
-describe('POST /story/like/:_id', () => {
+describe.only('POST /story/like/:_id', () => {
     let token1, idUser1, token2, idUser2, storyId;
 
     beforeEach('Sign up a user for test', async () => {
@@ -26,14 +26,65 @@ describe('POST /story/like/:_id', () => {
         const response = await request(app)
             .post('/story/like/' + storyId)
             .set({ token: token2 });
-        console.log(response.body);
-        // equal(response.status, 200);
-        // const { success, story } = response.body;
-        // equal(success, true);
-        // equal(story.content, 'abcd');
-        // const storyDb = await Story.findOne({}).populate('author');
-        // equal(storyDb, null);
-        // const user1 = await User.findById(idUser1);
-        // equal(user1.stories.length, 0);
+        equal(response.status, 200);
+        const { success, story } = response.body;
+        equal(success, true);
+        equal(story.fans[0], idUser2);
+        const storyDb = await Story.findOne({}).populate('fans');
+        equal(storyDb.fans[0].name, 'ti');
+    });
+
+    it('Cannot like story with invalid _id', async () => {
+        const response = await request(app)
+            .post('/story/like/' + 123)
+            .set({ token: token2 });
+        equal(response.status, 400);
+        const { success, story, message } = response.body;
+        equal(success, false);
+        equal(story, undefined);
+        equal(message, 'INVALID_ID');
+        const storyDb = await Story.findOne({}).populate('fans');
+        equal(storyDb.fans.length, 0);
+    });
+
+    it('Cannot like story with invalid token', async () => {
+        const response = await request(app)
+            .post('/story/like/' + storyId)
+            .set({ token: 'a.b.c' });
+        equal(response.status, 400);
+        const { success, story, message } = response.body;
+        equal(success, false);
+        equal(story, undefined);
+        equal(message, 'INVALID_TOKEN');
+        const storyDb = await Story.findOne({}).populate('fans');
+        equal(storyDb.fans.length, 0);
+    });
+
+    it('Cannot like story twice', async () => {
+        await StoryService.likeStory(idUser2, storyId);
+        const response = await request(app)
+            .post('/story/like/' + storyId)
+            .set({ token: token2 });
+        equal(response.status, 404);
+        const { success, story, message } = response.body;
+        equal(success, false);
+        equal(story, undefined);
+        equal(message, 'CANNOT_FIND_STORY');
+        const storyDb = await Story.findOne({}).populate('fans');
+        equal(storyDb.fans[0].name, 'ti');
+    });
+
+    it('Cannot like a removed story', async () => {
+        await StoryService.removeStory(idUser1, storyId);
+        const response = await request(app)
+            .post('/story/like/' + storyId)
+            .set({ token: token2 });
+        equal(response.status, 404);
+        const { success, story, message } = response.body;
+        equal(success, false);
+        equal(story, undefined);
+        equal(message, 'CANNOT_FIND_STORY');
+        const storyDb = await Story.findOne({}).populate('fans');
+        equal(storyDb, null);
     });
 });
