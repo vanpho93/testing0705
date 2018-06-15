e6d975e98d49a76adf774ea26d7a491cd0b2fd7c
Van Pho Nguyen <vanpho02@gmail.com>
2018-06-15 20:23:49 +0700

    test dislike story

--- test/story/dislikeStory.test.js
+++ test/story/dislikeStory.test.js
@@ -6,7 +6,7 @@ const { Story } = require('../../src/models/story.model');
 const { UserService } = require('../../src/services/user.service');
 const { StoryService } = require('../../src/services/story.service');
 
-describe('POST /story/dislike/:_id', () => {
+describe.only('POST /story/dislike/:_id', () => {
     let token1, idUser1, token2, idUser2, storyId;
 
     beforeEach('Sign up a user for test', async () => {
@@ -27,14 +27,79 @@ describe('POST /story/dislike/:_id', () => {
         const response = await request(app)
             .post('/story/dislike/' + storyId)
             .set({ token: token2 });
-        console.log(response.body);
-        // equal(response.status, 200);
-        // const { success, story } = response.body;
-        // equal(success, true);
-        // equal(story.content, 'abcd');
-        // const storyDb = await Story.findOne({}).populate('author');
-        // equal(storyDb, null);
-        // const user1 = await User.findById(idUser1);
-        // equal(user1.stories.length, 0);
+        equal(response.status, 200);
+        const { success, story, message } = response.body;
+        equal(success, true);
+        equal(story.fans.length, 0);
+        equal(message, undefined);
+        const storyDb = await Story.findOne({});
+        equal(storyDb.fans.length, 0);
+    });
+
+    it('Cannot dislike story with invalid id', async () => {
+        const response = await request(app)
+            .post('/story/dislike/' + 123)
+            .set({ token: token2 });
+        equal(response.status, 400);
+        const { success, story, message } = response.body;
+        equal(success, false);
+        equal(story, undefined);
+        equal(message, 'INVALID_ID');
+        const storyDb = await Story.findOne({});
+        equal(storyDb.fans.length, 1);
+    });
+
+    it('Cannot dislike story with invalid token', async () => {
+        const response = await request(app)
+            .post('/story/dislike/' + storyId)
+            .set({ token: 'a.b.c' });
+        equal(response.status, 400);
+        const { success, story, message } = response.body;
+        equal(success, false);
+        equal(story, undefined);
+        equal(message, 'INVALID_TOKEN');
+        const storyDb = await Story.findOne({});
+        equal(storyDb.fans.length, 1);
+    });
+
+    it('Cannot dislike story twice', async () => {
+        await StoryService.dislikeStory(idUser2, storyId);
+        const response = await request(app)
+            .post('/story/dislike/' + storyId)
+            .set({ token: token2 });
+        equal(response.status, 404);
+        const { success, story, message } = response.body;
+        equal(success, false);
+        equal(story, undefined);
+        equal(message, 'CANNOT_FIND_STORY');
+        const storyDb = await Story.findOne({});
+        equal(storyDb.fans.length, 0);
+    });
+
+    it('Cannot dislike story with token 1', async () => {
+        const response = await request(app)
+            .post('/story/dislike/' + storyId)
+            .set({ token: token1 });
+        equal(response.status, 404);
+        const { success, story, message } = response.body;
+        equal(success, false);
+        equal(story, undefined);
+        equal(message, 'CANNOT_FIND_STORY');
+        const storyDb = await Story.findOne({});
+        equal(storyDb.fans.length, 1);
+    });
+
+    it('Cannot dislike a removed story', async () => {
+        await StoryService.removeStory(idUser1, storyId);
+        const response = await request(app)
+            .post('/story/dislike/' + storyId)
+            .set({ token: token2 });
+        equal(response.status, 404);
+        const { success, story, message } = response.body;
+        equal(success, false);
+        equal(story, undefined);
+        equal(message, 'CANNOT_FIND_STORY');
+        const storyDb = await Story.findOne({});
+        equal(storyDb, null);
     });
 });
